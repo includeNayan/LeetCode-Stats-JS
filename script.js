3fd169c8f680e93fb5f9e26c4cfa4e6c0b0bdb9b
Nayan Jyoti Nath <nayannath2142@gmail.com>
2025-04-10 12:07:34 +0530

    2nd

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-document.addEventListener("DOMContentLoaded",function() {
-    document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
         const Searchbutton = document.getElementById("search");
         const Userinput = document.getElementById("userinput");
-        const Statcontainer = document.querySelector("statcontainer");
-        const easycircle = document.querySelector("easy");
-        const medcircle = document.querySelector("medium");
-        const hardcircle = document.querySelector("hard");
+        const Statcontainer = document.querySelector(".statcontainer");
+        const easycircle = document.querySelector(".easy");
+        const medcircle = document.querySelector(".medium");
+        const hardcircle = document.querySelector(".hard");
     
         const easyLabel = document.getElementById("easylabel");
         const medLabel = document.getElementById("mediumlabel");
         const hardLabel = document.getElementById("hardlabel");
-        const cardstat = document.querySelector("card");
+        const cardstat = document.querySelector(".cardst");
     
         // return true or false based on a regex
         function validate(username) {
@@ -19,7 +18,7 @@ document.addEventListener("DOMContentLoaded",function() {
                 alert("Username should not be empty");
                 return false;
             }
-            const regex = /^[a-zA-Z0-9_-]{1,20}$/;
+            const regex = /^[a-zA-Z0-9_-]{1,15}$/;
             const isMatch = regex.test(username);
             // checking the regex with the username
             if(!isMatch) {
@@ -30,37 +29,40 @@ document.addEventListener("DOMContentLoaded",function() {
     
 
         async function fetchDetails(username) {
+            const url = `https://leetcode-stats-api.herokuapp.com/${username}`;
             try{
                 Searchbutton.textContent = "Searching...";
                 Searchbutton.disabled = true;
                 //demo proxy url will request the lc server and server will acknowledge it
-                const proxyUrl = 'http://localhost:8080/';
-                const targetUrl = 'https://leetcode.com/graphql/';
-                // concatenated url proxyurl + targeturl
-                const myHeaders = new Headers();
-                myHeaders.append("content-type", "application/json");
+                // const targetUrl = 'https://leetcode.com/graphql/';
+                // // concatenated url proxyurl + targeturl
+                // const myHeaders = new Headers();
+                // myHeaders.append("content-type", "application/json");
     
-                const graphql = JSON.stringify({
-                    query: "\n    query userSessionProgress($username: String!) {\n  allQuestionsCount {\n    difficulty\n    count\n  }\n  matchedUser(username: $username) {\n    submitStats {\n      acSubmissionNum {\n        difficulty\n        count\n        submissions\n      }\n      totalSubmissionNum {\n        difficulty\n        count\n        submissions\n      }\n    }\n  }\n}\n    ",
-                    variables: { "username": `${username}` }
-                })
-                const requestOptions = {
-                    method: "POST", 
-                    headers: myHeaders,
-                    body: graphql,
-                    redirect: "follow"
-                };
-                const response = await fetch(proxyUrl+targetUrl,requestOptions);
+                // const graphql = JSON.stringify({
+                //     query: "\n    query userSessionProgress($username: String!) {\n  allQuestionsCount {\n    difficulty\n    count\n  }\n  matchedUser(username: $username) {\n    submitStats {\n      acSubmissionNum {\n        difficulty\n        count\n        submissions\n      }\n      totalSubmissionNum {\n        difficulty\n        count\n        submissions\n      }\n    }\n  }\n}\n    ",
+                //     variables: { "username": `${username}` }
+                // })
+                // const requestOptions = {
+                //     method: "POST", 
+                //     headers: myHeaders,
+                //     body: graphql,
+                //     redirect: "follow"
+                // };
+                const response = await fetch(url);
                 if(!response.ok) {
                     throw new Error("Unable to fetch the user details");
                 }
                 const parsedData = await response.json();
+                if (parsedData.status === "error") {
+                    throw new Error("User not found");
+                }
                 console.log("Logging data: ",parsedData);
                 displayData(parsedData);
-                statscnt.classList.remove("random");
+                Statcontainer.classList.remove("random");
             }
             catch(error) {
-                statscnt.innerHTML = `<p>No Data Found</p>`;
+                Statcontainer.innerHTML = `<p>No Data Found</p>`;
             }
             finally {
                 Searchbutton.textContent = "Search";
@@ -77,35 +79,34 @@ document.addEventListener("DOMContentLoaded",function() {
         }
 
         function displayData(parsedData) {
-            const total = parsedData.data.allQuestionsCount[0].count;
-            const easytotal = parsedData.data.allQuestionsCount[1].count;
-            const medtotal = parsedData.data.allQuestionsCount[2].count;
-            const hardtotal = parsedData.data.allQuestionsCount[3].count;
+            const total = parsedData.totalQuestions;
+            const easytotal = parsedData.totalEasy;
+            const medtotal = parsedData.totalMedium;
+            const hardtotal = parsedData.totalHard;
     
-            const soltotal = parsedData.data.matchedUser.submitStats.acSubmissionNum[0].count;
-            const soleasy = parsedData.data.matchedUser.submitStats.acSubmissionNum[1].count;
-            const solmed = parsedData.data.matchedUser.submitStats.acSubmissionNum[2].count;
-            const solhard = parsedData.data.matchedUser.submitStats.acSubmissionNum[3].count;
+            const soltotal = parsedData.totalSolved;
+            const soleasy = parsedData.easySolved;
+            const solmed = parsedData.mediumSolved;
+            const solhard = parsedData.hardSolved;
             updateProgress(soleasy,easytotal,easyLabel,easycircle);
             updateProgress(solmed,medtotal,medLabel,medcircle);
             updateProgress(solhard,hardtotal,hardLabel,hardcircle);
     
             const cardsData = [
-                {label : "Overall Submissions", value: parsedData.data.matchedUser.submitStats.totalSubmissionNum[0].submissions},
-                {label : "Overall Easy Submissions", value: parsedData.data.matchedUser.submitStats.totalSubmissionNum[1].submissions},
-                {label : "Overall Medium Submissions", value: parsedData.data.matchedUser.submitStats.totalSubmissionNum[2].submissions},
-                {label : "Overall Hard Submissions", value: parsedData.data.matchedUser.submitStats.totalSubmissionNum[3].submissions},
+                {label : "Acceptance Rate:", value: parsedData.acceptanceRate},
+                {label : "Contribution Points:", value: parsedData.contributionPoints},
+                {label : "Global Ranking:", value: parsedData.ranking},
+                {label : "Reputations:", value: parsedData.reputation},
             ];
             console.log("Card ka data: ", cardsData);
     
-            cardstatscnt.innerHTML = cardsData.map(
-                data => {
-                    return `
+            cardstat.innerHTML = cardsData.map(
+                data => `
                     <div class = "card">
                     <h4>${data.label}</h4>
                     <p>${data.value}</p>
-                    </div>`
-                }
+                    </div>
+                `
             ).join("")
     
         }
@@ -118,7 +119,5 @@ document.addEventListener("DOMContentLoaded",function() {
                 fetchDetails(username);
             }
         })
-    
-    
+
     })
-})
